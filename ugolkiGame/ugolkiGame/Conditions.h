3adee9712ad2d76cdd40b6138b13dafdd0855b15
mafiozy39 <57320920+mafiozy39@users.noreply.github.com>
2021-11-26 15:27:11 +0300

    Add files via upload

---
+++ ugolkiGame/ugolkiGame/Conditions.h
@@ -0,0 +1,134 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Conditions
+{
+public:
+	bool isOutOfBorders;
+	int tempX, tempY, currentImaginaryCell;
+	int tempColor;
+	int clickOnFigure(int x, int y, int cell[8][8],int cell_imaginary[8][8]) {
+		tempColor = cell[x][y];
+		tempX = x;
+		tempY = y;
+		currentImaginaryCell = cell_imaginary[x][y];
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				if (cell[i][j] == 3) cell[i][j] = 2;//убираю кресты при клике на другую пешку
+			}
+		}
+		if ((cell[x + 1][y] == 2) && (cell[x][y + 1] == 2) && (cell[x - 1][y] == 2) && (cell[x][y - 1] == 2)) {
+			cell[x + 1][y] = 3;
+			cell[x - 1][y] = 3;
+			isOutOfBorders = outOfBottomBorders(currentImaginaryCell);
+			if (isOutOfBorders == false) cell[x][y + 1] = 3;
+			isOutOfBorders = outOfTopBorders(currentImaginaryCell);
+			if (isOutOfBorders == false) cell[x][y - 1] = 3;
+		}
+		if ((cell[x + 1][y] == 2) && (cell[x][y + 1] == 2) && (cell[x - 1][y] == 2)) {
+			cell[x + 1][y] = 3;
+			cell[x - 1][y] = 3;
+			isOutOfBorders = outOfBottomBorders(currentImaginaryCell);
+			if (isOutOfBorders == false) cell[x][y + 1] = 3;
+		}
+		if ((cell[x + 1][y] == 2) && (cell[x][y + 1] == 2) && (cell[x][y - 1] == 2)) {
+			cell[x + 1][y] = 3;
+			isOutOfBorders = outOfBottomBorders(currentImaginaryCell);
+			if (isOutOfBorders == false) cell[x][y + 1] = 3;
+			isOutOfBorders = outOfTopBorders(currentImaginaryCell);
+			if (isOutOfBorders == false) cell[x][y - 1] = 3;
+		}
+		if ((cell[x + 1][y] == 2) && (cell[x - 1][y] == 2) && (cell[x][y - 1] == 2)) {
+			cell[x + 1][y] = 3;
+			cell[x - 1][y] = 3;
+			isOutOfBorders = outOfTopBorders(currentImaginaryCell);
+			if (isOutOfBorders == false) cell[x][y - 1] = 3;
+		}
+		if ((cell[x][y + 1] == 2) && (cell[x - 1][y] == 2) && (cell[x][y - 1] == 2)) {
+			cell[x - 1][y] = 3;
+			isOutOfBorders = outOfBottomBorders(currentImaginaryCell);
+			if (isOutOfBorders == false) cell[x][y + 1] = 3;
+			isOutOfBorders = outOfTopBorders(currentImaginaryCell);
+			if (isOutOfBorders == false) cell[x][y - 1] = 3;
+		}
+		if ((cell[x - 1][y] == 2) && (cell[x][y - 1] == 2)) {
+			cell[x - 1][y] = 3;
+			isOutOfBorders = outOfTopBorders(currentImaginaryCell);
+			if (isOutOfBorders == false) cell[x][y - 1] = 3;
+		}
+		if ((cell[x - 1][y] == 2) && (cell[x + 1][y] == 2)) {
+			cell[x - 1][y] = 3;
+			cell[x + 1][y] = 3;
+		}
+		if ((cell[x - 1][y] == 2) && (cell[x][y + 1] == 2)) {
+			cell[x - 1][y] = 3;
+			isOutOfBorders = outOfBottomBorders(currentImaginaryCell);
+			if (isOutOfBorders == false) cell[x][y + 1] = 3;
+		}
+		if ((cell[x][y - 1] == 2) && (cell[x + 1][y] == 2)) {
+			isOutOfBorders = outOfTopBorders(currentImaginaryCell);
+			if (isOutOfBorders == false) cell[x][y - 1] = 3;
+			cell[x + 1][y] = 3;
+		}
+		if ((cell[x][y - 1] == 2) && (cell[x][y + 1] == 2)) {
+			isOutOfBorders = outOfTopBorders(currentImaginaryCell);
+			if (isOutOfBorders == false) cell[x][y - 1] = 3;
+			isOutOfBorders = outOfBottomBorders(currentImaginaryCell);
+			if (isOutOfBorders == false) cell[x][y + 1] = 3;
+		}
+		if ((cell[x + 1][y] == 2) && (cell[x][y + 1] == 2)) {
+			cell[x + 1][y] = 3;
+			isOutOfBorders = outOfBottomBorders(currentImaginaryCell);
+			if (isOutOfBorders == false) cell[x][y + 1] = 3;
+		}
+		if (cell[x - 1][y] == 2) cell[x - 1][y] = 3;
+
+		if (cell[x][y - 1] == 2) {
+			isOutOfBorders = outOfTopBorders(currentImaginaryCell);
+			if (isOutOfBorders == false) cell[x][y - 1] = 3;
+		}
+		if (cell[x + 1][y] == 2) cell[x + 1][y] = 3;
+		if (cell[x][y + 1] == 2) {
+			isOutOfBorders = outOfBottomBorders(currentImaginaryCell);
+			if (isOutOfBorders == false) cell[x][y + 1] = 3;
+		}
+		return cell[x][y];
+	}
+	int clickOnSpot(int cell[8][8],int x, int y) {
+		cell[x][y] = tempColor;
+		cell[tempX][tempY] = 2;
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				if (cell[i][j] == 3) cell[i][j] = 2;//убираю кресты после нажатия
+			}
+		}
+		return cell[x][y];
+	}
+	int cancelSpots(int x, int y, int cell[8][8]) {
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				if (cell[i][j] == 3) cell[i][j] = 2;//убираю кресты при клике на пустую клетку
+			}
+		}
+		return cell[x][y];
+	}
+	int outOfTopBorders(int cic) {
+		if ((cic == 0) || (cic == 8) || (cic == 16) || (cic == 24) || (cic == 32) || (cic == 40) || (cic == 48) || (cic == 56)) return true;
+		else return false;
+	}
+	int outOfBottomBorders(int cic) {
+		if ((cic == 7) || (cic == 15) || (cic == 23) || (cic == 31) || (cic == 39) || (cic == 47) || (cic == 55) || (cic == 63)) return true;
+		else return false;
+	}
+	int whitesWin(int cell[8][8]) {
+		if ((cell[0][0] == 0) && (cell[0][1] == 0) && (cell[0][2] == 0) && (cell[1][0] == 0) && (cell[1][1] == 0) && (cell[1][2] == 0) && (cell[2][0] == 0) && (cell[2][1] == 0) && (cell[2][2] == 0)) {
+			return true;
+		}
+	}
+	int blacksWin(int cell[8][8]) {
+		if ((cell[5][5] == 1) && (cell[5][6] == 1) && (cell[5][7] == 1) && (cell[6][5] == 1) && (cell[6][6] == 1) && (cell[6][7] == 1) && (cell[7][5] == 1) && (cell[7][6] == 1) && (cell[7][7] == 1)) {
+			return true;
+		}
+	}
+};
---
+++ ugolkiGame/ugolkiGame/Source.cpp
@@ -0,0 +1,101 @@
+#include <SFML/Graphics.hpp>
+#include "Conditions.h"
+using namespace sf;
+using namespace std;
+
+
+
+int cell_width = 100;
+int cell[8][8];
+int cell_imaginary[8][8];
+int counter = 0;
+int movement=1;
+Conditions cond{};
+int main()
+{
+	Texture t;
+	t.loadFromFile("desk.png");
+	Sprite s(t);
+	Texture t1;
+	t1.loadFromFile("white.png");
+	Sprite s1(t1);
+	Texture t2;
+	t2.loadFromFile("black.png");
+	Sprite s2(t2);
+	Texture t3;
+	t3.loadFromFile("possibleMove.png");
+	Sprite s3(t3);
+	Texture ww;
+	ww.loadFromFile("ww.jpg");
+	Sprite whitewin(ww);
+	whitewin.setPosition(200, 200);
+	Texture bw;
+	bw.loadFromFile("bw.jpg");
+	Sprite blackwin(bw);
+	blackwin.setPosition(200, 200);
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			cell[i][j] = 2;
+			if ((i == 0) && (j < 3)) cell[i][j] = 1;
+			if ((i == 1) && (j < 3)) cell[i][j] = 1;
+			if ((i == 2) && (j < 3)) cell[i][j] = 1;
+			if ((i == 5) && (j > 4)) cell[i][j] = 0;
+			if ((i == 6) && (j > 4)) cell[i][j] = 0;
+			if ((i == 7) && (j > 4)) cell[i][j] = 0;
+			cell_imaginary[i][j] = counter;
+			counter++;
+		}
+	}
+	RenderWindow window(VideoMode(875, 875), "Ugolki");
+
+	while (window.isOpen())
+	{
+		Vector2i pos = Mouse::getPosition(window);
+		int x = pos.x / cell_width;
+		int y = pos.y / cell_width;
+
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+				window.close();
+			if(event.type==Event::MouseButtonPressed)
+				if ((event.key.code == Mouse::Left) && (cell[x][y] < 2)){
+					if ((cell[x][y] == 0) && (movement == 1)) cell[x][y] = cond.clickOnFigure(x, y, cell,cell_imaginary);
+					if ((cell[x][y] == 1) && (movement == -1))cell[x][y] = cond.clickOnFigure(x, y, cell, cell_imaginary);
+				}
+				if ((event.key.code == Mouse::Left) && (cell[x][y] == 3)) {
+					cell[x][y]=cond.clickOnSpot(cell,x,y);
+					movement = -1 * movement;
+				}
+				if ((event.key.code == Mouse::Left) && (cell[x][y] == 2)) {
+					cell[x][y] = cond.cancelSpots(x, y, cell);	
+				}
+		}
+		window.clear(Color(229,189,140));
+		window.draw(s);
+
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				if (cell[i][j] == 0) {
+					s1.setPosition(i * cell_width, j * cell_width);
+					window.draw(s1);
+				}
+				if (cell[i][j] == 1) {
+					s2.setPosition(i * cell_width, j * cell_width);
+					window.draw(s2);
+				}
+				if (cell[i][j] == 3) {
+					s3.setPosition(i * cell_width, j * cell_width);
+					window.draw(s3);
+				}
+			}
+		}
+		if(cond.whitesWin(cell)==true) window.draw(whitewin);
+		if(cond.blacksWin(cell)==true) window.draw(blackwin);
+
+		window.display();
+	}
+
+	return 0;
+}
