832dcacf51495ed0e9155025b6e1247334001045
mafiozy39 <57320920+mafiozy39@users.noreply.github.com>
2021-11-30 22:50:25 +0300

    Add files via upload

--- ugolkiGame/ugolkiGame/Source.cpp
+++ ugolkiGame/ugolkiGame/Source.cpp
@@ -1,5 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include "Conditions.h"
+#include "bot.h"
 using namespace sf;
 using namespace std;
 
@@ -9,8 +10,9 @@ int cell_width = 100;
 int cell[8][8];
 int cell_imaginary[8][8];
 int counter = 0;
-int movement=1;
+int movement = 1;
 Conditions cond{};
+Bot ai{};
 int main()
 {
 	Texture t;
@@ -47,32 +49,37 @@ int main()
 		}
 	}
 	RenderWindow window(VideoMode(875, 875), "Ugolki");
-
 	while (window.isOpen())
 	{
+		if (movement == -1) {
+			ai.move(cell);
+			movement = -1 * movement;
+		}
 		Vector2i pos = Mouse::getPosition(window);
 		int x = pos.x / cell_width;
 		int y = pos.y / cell_width;
-
+		if ((x > 7)||(y>7)) continue;
 		Event event;
 		while (window.pollEvent(event))
 		{
 			if (event.type == Event::Closed)
 				window.close();
-			if(event.type==Event::MouseButtonPressed)
-				if ((event.key.code == Mouse::Left) && (cell[x][y] < 2)){
-					if ((cell[x][y] == 0) && (movement == 1)) cell[x][y] = cond.clickOnFigure(x, y, cell,cell_imaginary);
-					if ((cell[x][y] == 1) && (movement == -1))cell[x][y] = cond.clickOnFigure(x, y, cell, cell_imaginary);
-				}
-				if ((event.key.code == Mouse::Left) && (cell[x][y] == 3)) {
-					cell[x][y]=cond.clickOnSpot(cell,x,y);
-					movement = -1 * movement;
-				}
-				if ((event.key.code == Mouse::Left) && (cell[x][y] == 2)) {
-					cell[x][y] = cond.cancelSpots(x, y, cell);	
-				}
+			if (event.type == Event::MouseButtonPressed)
+			if ((event.key.code == Mouse::Left) && (cell[x][y] < 2)) {
+					cout << "cell_imaginary: " << cell_imaginary[x][y] << "|cell: " << cell[x][y] << "|x: " << x << "|y: " << y << endl;
+					if ((cell[x][y] == 0) && (movement == 1)) cell[x][y] = cond.clickOnFigure(x, y, cell, cell_imaginary);
+					//if ((cell[x][y] == 1) && (movement == -1))cell[x][y] = cond.clickOnFigure(x, y, cell, cell_imaginary);
+			}
+			if ((event.key.code == Mouse::Left) && (cell[x][y] == 3)) {
+				cell[x][y] = cond.clickOnSpot(cell, x, y);
+				movement = -1 * movement;
+			}
+			if ((event.key.code == Mouse::Left) && (cell[x][y] == 2)) {
+				cell[x][y] = cond.cancelSpots(x, y, cell);
+				cout << "cell_imaginary: " << cell_imaginary[x][y] << "|cell: " << cell[x][y] << "|x: " << x << "|y: " << y << endl;
+			}
 		}
-		window.clear(Color(229,189,140));
+		window.clear(Color(229, 189, 140));
 		window.draw(s);
 
 		for (int i = 0; i < 8; i++) {
@@ -91,8 +98,8 @@ int main()
 				}
 			}
 		}
-		if(cond.whitesWin(cell)==true) window.draw(whitewin);
-		if(cond.blacksWin(cell)==true) window.draw(blackwin);
+		if (cond.whitesWin(cell) == true) window.draw(whitewin);
+		if (cond.blacksWin(cell) == true) window.draw(blackwin);
 
 		window.display();
 	}
